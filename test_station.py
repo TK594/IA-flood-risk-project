5e6c275fb144633758000c3f6a818850fa2ce1a3
Tae Kang <tk594@cam.ac.uk>
2022-02-25 01:45:21 +0000

    tested relative water level

--- test_station.py
+++ test_station.py
@@ -43,3 +43,15 @@ def test_inconsistent_typical_range_consistent():
     station_C = MonitoringStation('ID C', 'Measurement ID C', 'Name C', (6,3), (5,4), 'river 2', 'Town 3')
 
     assert inconsistent_typical_range_stations((station_A, station_B, station_C)) == ['Name A', 'Name C']
+
+def test_relative_water_level():
+    station_A = MonitoringStation('ID A', 'Measurement ID A', 'Name A', (0,1), None, 'river 1', 'Town 1')
+    station_B = MonitoringStation('ID B', 'Measurement ID B', 'Name B', (5,5), (1,3), 'river 2', 'Town 2')
+    station_C = MonitoringStation('ID C', 'Measurement ID C', 'Name C', (6,3), (5,4), 'river 2', 'Town 3')
+    station_A.latest_level = 0.5
+    station_B.latest_level = 2.4
+    station_C.latest_level = 4.5
+
+    assert station_A.relative_water_level() == None
+    assert station_B.relative_water_level() == 0.7
+    assert station_C.relative_water_level() == None
