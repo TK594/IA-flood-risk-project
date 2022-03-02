68d379e1d7d125226fbab1021f5ebffe6948836a
rdf1309 <rdf33@cam.ac.uk>
2022-03-02 11:30:58 +0000

    fix?

--- Task2G.py
+++ Task2G.py
@@ -1,6 +1,6 @@
 import numpy as np
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 import datetime
 from floodsystem.analysis import polyfit
@@ -13,11 +13,12 @@ def run():
     #build list of stations     
     stations = build_station_list()
     #get relative levels of stations above a low tolerance
+    update_water_levels(stations)
     relative_levels = stations_level_over_threshold(stations, 0.6)
 
 
 
-    low = [] #stations with a relative level of 0.6-0.8 with an increasing relative level
+    low = [] #stations with a relative level of 0.6 or higher with a decreasing or constant relative level
     moderate = [] #stations with a relative level of 0.8 or larger that have an increasing relative level
     high = [] #stations with a relative level between 1 and 1.5
     severe = [] #stations with a relative level above 1.5
@@ -31,16 +32,19 @@ def run():
         elif level>1:
             high.append(station.name)
             dates, levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days =2))
-            x = matplotlib.dates.date2num(dates)     
-            poly, d0 = polyfit(dates, levels, 4)
-            dydx = poly.deriv()
+            try:
+                x = matplotlib.dates.date2num(dates)     
+                poly, d0 = polyfit(dates, levels, 4)
+                dydx = poly.deriv()
+            except:
+                continue
 
         
         
         elif dydx(x[-1]-d0) > 0 and level >0.8:
             moderate.append(station.name)
 
-        elif dydx(x[-1]-d0)>0:
+        else:
             low.append(station.name)
 
         
